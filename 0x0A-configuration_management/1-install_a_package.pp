885ed6dd149734065980cb8d9de10fa97ab223ef
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-02-24 00:54:38 +0100

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,25 @@
-# Using Puppet, install flask from pip3.
+#!/usr/bin/pup
+# install flask from pip3
 
-package { 'pip3':
-  ensure => installed,
+package { 'python3.8':
+  ensure => '3.8.10',
 }
 
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
