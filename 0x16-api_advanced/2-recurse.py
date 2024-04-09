1ebbd70275eed828b11c648a03f67340057c2554
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-04-09 22:00:18 +0000

    task-02

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module contains function that queries the Reddit API
+"""
+import requests
+
+
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Print the top ten hot posts of a subreddit recursively """
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False,
+    )
+
+    if res.status_code != 200:
+        return None
+
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
