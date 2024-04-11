aaad729f4c3d90c737e1359b3df99d63aeebdd68
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-04-11 17:35:03 +0000

    what the f

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+this doc for module
 """
-
 import requests
+
+header = {
+    "User-Agent": "MyCustomUserAgent/1.0"
+}
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """method doc"""
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        response = requests.get(url, allow_redirects=False, headers=header)
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    except Exception:
+        return 0
